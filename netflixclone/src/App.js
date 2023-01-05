701216d7815a29249a713e69a0f1c7de3ea12770
nathslopess <nathalia-silvalopes@outlook.com>
2023-01-05 14:21:00 -0300

    Iniciando o desenvolvimento da page

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from  'react'
+import Tmdb from './api/Tmdb'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default () => {
+
+  const [movieList, setMovieList] = useState([]) 
+
+  useEffect( () => {
+    const loadAll = async () => {
+      //Pegando a lista TOTAL 
+      let list = await Tmdb.getHomeList()
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
+  return(
+    <div className='page'>
+      Header
+      Destaque 
+      As Listas diversas
     </div>
-  );
+  )
 }
-
-export default App;
